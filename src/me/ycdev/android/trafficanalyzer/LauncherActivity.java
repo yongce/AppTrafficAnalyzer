f2a6ba00e0dfa0493d9b6a34b860278581490558
Yongce Tu <yongce.tu@gmail.com>
2014-04-22 19:07:29 +0800

    change: Create empty Android project

---
+++ src/me/ycdev/android/trafficanalyzer/LauncherActivity.java
@@ -0,0 +1,15 @@
+package me.ycdev.android.trafficanalyzer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LauncherActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
